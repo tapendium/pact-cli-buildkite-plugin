2d0aa0897d6b7c5b89aaefaceac3ebf390fff6e2
Pascal Heeraman <smilin-desperado@users.noreply.github.com>
2023-12-21 15:55:34 +1100

    [LIBS-233] Insert trigger step for verification pipelines (#3)
    * [LIBS-233] Insert trigger step for verification pipelines
    
    * [LIBS-233] Run in post-command hook
    
    * [LIBS-233] Pass graphql api details and build url
    
    * [LIBS-233] Mount bk agent and pass required env vars

---
+++ lib/buildkite.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Get validate pipeline from Buildkite Graphql API
+function get_verification_pipeline {
+	local repo="$1"
+	local api_url="$2"
+	local token="$3"
+	local queryData='{"query":"{organization(slug:\"tapendium\"){pipelines(first:10,repository:{url:\"'"${repo}"'\"}){count edges{node{name slug}}}}}"}'
+
+	if ! response=$(
+		curl -s "$api_url" \
+			--request POST \
+			--header "Content-Type: application/json" \
+			--header "Authorization: Bearer $token" \
+			--data-raw "${queryData}"
+	); then
+		log "Unable to fetch Buildkite Validate pipeline"
+		exit 1
+	fi
+
+	slug="$(jq -nr --argjson r "${response}" '$r.data.organization.pipelines.edges | .[] | select(.node.slug | test("validate")) | .node.slug')"
+
+	echo "$slug"
+}
+
+# Generate a trigger step for pipeline
+function generate_trigger_step {
+	local provider="$1"
+	local pacticipant="$2"
+	local pipelineSlug="$3"
+
+	cat <<EOF
+  - label: ":pact: Trigger provider verification for $provider"
+    trigger: "$pipelineSlug"
+    build:
+      branch: main
+      env:
+        PACT_PACTICIPANT: "${pacticipant}"
+        TRIGGER_REASON: "pact_contract_requires_verification" 
+        TRIGGER_BUILD_URL: "${BUILDKITE_BUILD_URL}"
+
+EOF
+}
--- lib/run.sh
+++ lib/run.sh
@@ -6,6 +6,7 @@ CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 . "$CWD/utils.sh"
 . "$CWD/pact.sh"
+. "$CWD/buildkite.sh"
 
 if [[ "${BUILDKITE_PLUGIN_PACT_CLI_DEBUG:-false}" =~ (true|on|1) ]]; then
 	echo "~~~ :hammer: Enabling debug mode"
@@ -30,16 +31,38 @@ function update_pacts {
 	local main_branch="$(plugin_get_var MAIN_BRANCH main)"
 	local version="$(plugin_get_var VERSION $BUILDKITE_COMMIT)"
 	assert_var version
-	local branch="$(plugin_get_var BRANCH $BUILDKITE_BRANCH)"
+	local branch="$(plugin_get_var BRANCH "$BUILDKITE_BRANCH")"
 	assert_var branch
 	local environment="$(plugin_get_var ENVIRONMENT production)"
 	local pact_dir="$(plugin_get_var PACTS_PATH pacts)"
 
 	if [ "$action" == "pr" ]; then
-		# Pull Request
+		# PR pipeline
+		#
+		# Access to Buildkite Graphql API is needed for retrieving verification pipelines
+		local bk_gql_url="$(plugin_get_var GRAPHQL_URL "https://graphql.buildkite.com/v1")"
+		assert_var BUILDKITE_GRAPHQL_API_TOKEN
+		assert_var BUILDKITE_BUILD_URL
+
 		upsert_pacticipant "$pacticipant" "$main_branch" "$repo_url"
 		publish_pacts "$version" "$pact_dir" "$branch"
+
+		declare -a providers=()
+		get_providers_to_check providers "$pacticipant" "$version" "$environment"
+
+		for provider in "${providers[@]}"; do
+			log "Reading repository url for $provider from Pact broker"
+			url="$(get_repo_url "$provider")"
+			log "Repo url for provider $provider: $url"
+			pipeline="$(get_verification_pipeline "$url" "$bk_gql_url" $"BUILDKITE_GRAPHQL_API_TOKEN")"
+			log "Found pipeline $pipeline"
+
+			trigger="$(generate_trigger_step "$provider" "$pacticipant" "$pipeline")"
+			echo "$trigger" | buildkite-agent pipeline upload
+		done
+
 	elif [ "$action" == "merge" ]; then
+		# Merge pipeline - Deploy to production
 		publish_pacts "$version" "$pact_dir" "$branch"
 		record_deployment "$pacticipant" "$version" "$environment"
 	else
