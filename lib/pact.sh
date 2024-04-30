1a7394e741738c489832682c620f38f77bdeaaf4
Pascal Heeraman <pascal@tapendium.com>
2024-04-30 12:09:34 +1000

    [LIBS-331] Fix formatting

--- lib/pact.sh
+++ lib/pact.sh
@@ -72,8 +72,8 @@ function get_providers_to_check {
 		--output json)"; then
 		log "Summary: $(jq -nr --argjson result "$result" '$result.summary.reason')"
 
-        # Find all provider services where this pacticipant is a consumer and
-        # no verification results exist.
+		# Find all provider services where this pacticipant is a consumer and
+		# no verification results exist.
 		readarray -t prov < <(jq -nr --argjson result "${result}" '$result.matrix | .[] | select(.consumer.name == '\""${pacticipant}"\"') | select(.verificationResult.success != true) | .provider.name')
 		log "Providers needing re-verification:"
 		log "${prov[@]}"
